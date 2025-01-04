d0bda76572a8377fa4d0a7cc3dae9bc3d58700df
Tariq Mehmood <tariqmalik323@gmail.com>
2025-01-04 21:42:50 +0500

    Update and rename k8s-ubuntu-worker.sh to k8s-ubuntu-cluster.sh

--- k8s-ubuntu-cluster.sh
+++ k8s-ubuntu-cluster.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# SCript for K8s cluster worker node.
+# Script for K8s cluster for every node with node.
 # Before running this ssh make sure you have changed your node's hostname and also configured static IP.
 
 # Switching to root user (Optinal)
 sudo -i
 
 # Disabling Swap feature on node
-swapoff -a 
+sudo swapoff -a 
 (crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
 
 
@@ -31,6 +31,10 @@ sudo sysctl --system
 
 
 # Installing containerd (for cintainer creation)
+
+# If the directory `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
+# sudo mkdir -p -m 755 /etc/apt/keyrings
+
 
 sudo apt-get update
 sudo apt-get install ca-certificates curl
@@ -63,9 +67,6 @@ sudo apt-get update
 
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
-# If the directory `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
-# sudo mkdir -p -m 755 /etc/apt/keyrings
-
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -78,4 +79,6 @@ sudo apt-mark hold kubelet kubeadm kubectl
 
 sudo systemctl enable --now kubelet
 
+echo "To setup Master node initialization you can follow steps mention in following link"
 
+echo "https://github.com/Tariq-Mehmood-Malik/Kubernetes-Cluster-Creation/blob/main/README.md#for-controller-only"
